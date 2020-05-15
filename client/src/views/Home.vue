4dd3d5321174c9eca9ade04e6f917449c176afd1
braisv <brais.vidal.rodriguez@gmail.com>
2020-05-15 21:49:54 +0200

    fetching villagers

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,17 +1,66 @@
 <template>
   <div class="home">
     <h1>AC!</h1>
+    <div class="villagers">
+      <div
+        class="villagers__card"
+        v-for="(villager, index) in villagers"
+        :key="index"
+      >
+        <div class="villagers__card-name">
+          <div class="villagers__card-name__spanish">
+            {{ villager.name["name-sp"] }}
+          </div>
+          <div class="villagers__card-name__english">
+            {{ villager.name["name-en"] }}
+          </div>
+          <div class="villagers__card-name__japanish">
+            {{ villager.name["name-jp"] }}
+          </div>
+        </div>
+
+        <div class="villagers__card-image">
+          {{ villager.image }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      villagers: [],
+    };
+  },
+  created() {
+    this.getVillagers();
+  },
+  methods: {
+    async getVillagers() {
+      let doc = await this.axios
+        .get("/villagers")
+        .then((res) => {
+          return res.data;
+        })
+        .catch((err) => console.log(err));
+
+      doc.map(async (el) => {
+        let img = await this.axios
+          .get(`/images/villagers/${el.id}`)
+          .then((resp) => {
+            return resp.data;
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+        el.img = img;
+      });
+      this.villagers = doc;
+      console.log("VILLAGERS: ", this.villagers);
+    },
   },
 };
 </script>
